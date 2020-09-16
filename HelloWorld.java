c9f7577f90cfeb47cc51597383a26fb3c476e096
Singhal-harsh <singhal.harsh2@gmail.com>
2020-09-16 11:38:51 +0000

    New Hello World 7 times

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,8 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-    for (int i = 0; i < 5; i++) {
-	  System.out.println("Hello Citi from crazy experiment!"); 
+    for (int i = 0; i < 7; i++) {
+	  System.out.println("Hello Citi, 7 times!"); 
     }
   }
 }
